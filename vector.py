c1876f8b227fe57a43bb641b4aaeed1f433066c8
drago <drago@localhost.localdomain>
2024-08-20 15:53:14 +0300

    Added Vector2 functional for short infinity map

--- vector.py
+++ vector.py
@@ -2,9 +2,13 @@ class Vector2():
     x = 0
     y = 0
     
-    def __init__(self, xy_array : set|list) -> None:
+    def __init__(self, xy_array : set|list, limit_x:int=None, limit_y:int=None) -> None:
         self.x = xy_array[0]
         self.y = xy_array[1]
+        if (limit_y and limit_y<=0) or (limit_x and limit_x <= 0):
+            raise ValueError
+        self.limit_x = limit_x
+        self.limit_y = limit_y
         
     def __len__(self):
         return (self.x**2 + self.y**2) ** 0.5
@@ -25,5 +29,11 @@ class Vector2():
         return Vector2((self.x*k, self.y*k))
     
     def coords(self):
-        return (self.x, self.y)
+        x = self.x
+        y = self.y 
+        if self.limit_x:
+            x %= self.limit_x
+        if self.limit_y:
+            y %= self.limit_y
+        return (x, y)
     
